1bb8cb583dc9ee48be9cf3b113df6dae14f15729
RogueException <rogueexception@gmail.com>
2018-07-01 16:27:30 -0700

    Support DateTimes with fractions shorter than 7 digits

--- test/Voltaic.Serialization.Utf8.Tests/DateTime.cs
+++ test/Voltaic.Serialization.Utf8.Tests/DateTime.cs
@@ -177,11 +177,14 @@ namespace Voltaic.Serialization.Utf8.Tests
             yield return ReadWrite("0001-01-01T00:00:00.0000000+00:00", //Min
                 new DateTimeOffset(year: 1, month: 1, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0, offset:
                 new TimeSpan(hours: 0, minutes: 0, seconds: 0)));
-            yield return ReadWrite("2017-01-13T03:45:32.0550000+08:00",
-                new DateTimeOffset(year: 2017, month: 1, day: 13, hour: 3, minute: 45, second: 32, millisecond: 55, offset:
+            yield return ReadWrite("2017-01-13T03:45:32.0500000+08:00",
+                new DateTimeOffset(year: 2017, month: 1, day: 13, hour: 3, minute: 45, second: 32, millisecond: 50, offset:
                 new TimeSpan(hours: 8, minutes: 0, seconds: 0)));
-            yield return ReadWrite("2017-01-13T03:45:32.0550000-09:00",
-                new DateTimeOffset(year: 2017, month: 1, day: 13, hour: 3, minute: 45, second: 32, millisecond: 55, offset:
+            yield return ReadWrite("2017-01-13T03:45:32.0500000-09:00",
+                new DateTimeOffset(year: 2017, month: 1, day: 13, hour: 3, minute: 45, second: 32, millisecond: 50, offset:
+                new TimeSpan(hours: -9, minutes: 0, seconds: 0)));
+            yield return Read("2017-01-13T03:45:32.050000-09:00", // Shorter fraction
+                new DateTimeOffset(year: 2017, month: 1, day: 13, hour: 3, minute: 45, second: 32, millisecond: 5, offset:
                 new TimeSpan(hours: -9, minutes: 0, seconds: 0)));
             yield return ReadWrite("9999-12-31T11:59:59.9990000+00:00", //Max
                 new DateTimeOffset(year: 9999, month: 12, day: 31, hour: 11, minute: 59, second: 59, millisecond: 999, offset:
