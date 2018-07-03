e219a8210db69ae8598341aa3dad832dec741f63
RogueException <rogueexception@gmail.com>
2018-07-02 19:50:04 -0700

    Restrict EpochAttribute to properties

--- src/Voltaic.Serialization/Attributes/EpochAttribute.cs
+++ src/Voltaic.Serialization/Attributes/EpochAttribute.cs
@@ -9,6 +9,7 @@ namespace Voltaic.Serialization
         UnixSeconds
     }
 
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class EpochAttribute : Attribute
     {
         public EpochType Type { get; }
