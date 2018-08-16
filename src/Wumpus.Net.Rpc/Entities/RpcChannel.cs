dcd305a82a110a8a9896aa31803b35471fdf576d
RogueException <rogueexception@gmail.com>
2018-08-16 12:33:19 -0700

    RpcChannel should extend Channel

--- src/Wumpus.Net.Rpc/Entities/RpcChannel.cs
+++ src/Wumpus.Net.Rpc/Entities/RpcChannel.cs
@@ -4,29 +4,8 @@ using Voltaic.Serialization;
 namespace Wumpus.Entities
 {
     /// <summary> xxx </summary>
-    public class RpcChannel
+    public class RpcChannel : Channel
     {
-        /// <summary> xxx </summary>
-        [ModelProperty("id")]
-        public Snowflake Id { get; set; }
-        /// <summary> xxx </summary>
-        [ModelProperty("guild_id")]
-        public Optional<Snowflake> GuildId { get; set; }
-        /// <summary> xxx </summary>
-        [ModelProperty("name")]
-        public Optional<Utf8String> Name { get; set; }
-        /// <summary> xxx </summary>
-        [ModelProperty("type")]
-        public ChannelType Type { get; set; }
-        /// <summary> xxx </summary>
-        [ModelProperty("bitrate")]
-        public Optional<int> Bitrate { get; set; }
-        /// <summary> xxx </summary>
-        [ModelProperty("user_limit")]
-        public Optional<int> UserLimit { get; set; }
-        /// <summary> xxx </summary>
-        [ModelProperty("position")]
-        public Optional<int> Position { get; set; }
         /// <summary> xxx </summary>
         [ModelProperty("voice_states")]
         public VoiceState[] VoiceStates { get; set; }
